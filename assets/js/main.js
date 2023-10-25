f28ec01e1f2d9ac90ccd6d170f23d1a49111227e
Miguel <miguelmds925@gmail.com>
2023-10-24 21:12:33 -0300

    iniciando o Javascript

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+function init(apiUrl) {
+  function fetchParrotsCount() {
+    return fetch(apiUrl + "/messages/parrots-count")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(count) {
+        document.getElementById("parrots-counter").innerHTML = count;
+      });
+  }}
